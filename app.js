c6ab50f5b3daaba46090c1c01028e64b2d86f0bc
PragyaJollySingh7 <96084502+PragyaJollySingh7@users.noreply.github.com>
2022-01-24 10:59:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,8 @@ const app = express()
 const port = process.env.PORT
 
 app.get('/', (req, res) => res.send('Hello World!'))
-
+app.get('/logging', (req, res) => {
+    console.log(`logging`))
+    res.send('thanks')
+  })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
